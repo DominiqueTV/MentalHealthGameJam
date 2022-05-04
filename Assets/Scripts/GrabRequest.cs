ba268485baa1514d4e0a661db2f85c2b07879544
Danejw <djw.learn@gmail.com>
2022-05-04 11:12:00 -0700

    Imported and Implemented AutoHands

--- Assets/Scripts/GrabRequest.cs
+++ Assets/Scripts/GrabRequest.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.XR;
 using UnityEngine.XR.Interaction.Toolkit;
 using Normal.Realtime;
-
+using Autohand;
 
 namespace ETS.Realtime
 {
@@ -14,18 +14,29 @@ namespace ETS.Realtime
         private RealtimeTransform realtimeTransform;
         private RealtimeView realtimeView;
         private XRGrabInteractable grabInteractable;
+        private Grabbable grabbable;
 
         private void Start()
         {
             realtimeTransform = GetComponent<RealtimeTransform>();
             realtimeView = GetComponent<RealtimeView>();
             grabInteractable  = GetComponent<XRGrabInteractable>();
+            grabbable = GetComponent<Grabbable>();
         }
 
         private void Update()
         {
             // need to check if item is already being held
-            if (grabInteractable.isSelected)
+            if (grabInteractable && grabInteractable.isSelected)
+            {
+                if (!realtimeTransform.isOwnedRemotelySelf || realtimeTransform.isUnownedSelf)
+                {
+                    realtimeTransform.RequestOwnership();
+                    realtimeView.RequestOwnership();
+                }
+            }
+
+            if (grabbable && grabbable.IsHeld())
             {
                 if (!realtimeTransform.isOwnedRemotelySelf || realtimeTransform.isUnownedSelf)
                 {
